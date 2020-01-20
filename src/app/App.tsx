a1bbc7db06840ecc53e88367b9bcdbb4e83c7571
Pascal Jordin <jordin@sipgate.de>
2020-01-20 09:16:32 +0100

    :sparkle: add missing routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,24 @@
-import { ThemeProvider } from "@material-ui/styles";
-import { Router } from "@reach/router";
-import React from "react";
-import validate from "validate.js";
-import "./App.css";
-import { Dashboard, UserList, RouteWithLayout, Account, NotFound, ProductList } from "./components";
-import theme from "./theme";
-import { Main as MainLayout/*,  Minimal as MinimalLayout */ } from './layouts';
-import validators from "./utils/validators";
+import { ThemeProvider } from '@material-ui/styles';
+import { Router, Redirect } from '@reach/router';
+import React from 'react';
+import validate from 'validate.js';
+import './App.css';
+import {
+  Dashboard,
+  UserList,
+  RouteWithLayout,
+  Account,
+  NotFound,
+  ProductList,
+  Typography,
+  Icons,
+  Settings,
+  SignUp,
+  SignIn
+} from './components';
+import theme from './theme';
+import { Main as MainLayout /*,  Minimal as MinimalLayout */ } from './layouts';
+import validators from './utils/validators';
 
 // Chart.controllers.bar = Chart.controllers.bar.extend(chartjs);
 
@@ -19,11 +31,58 @@ const App: React.FC<{}> = () => {
   return (
     <ThemeProvider theme={theme}>
       <Router>
-        <RouteWithLayout path="/" layout={MainLayout} component={Dashboard} />
-        <RouteWithLayout path="/users" layout={MainLayout} component={UserList} />
-        <RouteWithLayout path="/account" layout={MainLayout} component={Account} />
-        <RouteWithLayout path="/products" layout={MainLayout} component={ProductList} />
-        <RouteWithLayout path="/notfound" layout={MainLayout} component={NotFound} />
+        <Redirect path="/" to="/dashboard" />
+        <RouteWithLayout
+          path="/dashboard"
+          layout={MainLayout}
+          component={Dashboard}
+        />
+        <RouteWithLayout
+          path="/users"
+          layout={MainLayout}
+          component={UserList}
+        />
+        <RouteWithLayout
+          path="/products"
+          layout={MainLayout}
+          component={ProductList}
+        />
+        <RouteWithLayout
+          path="/typography"
+          layout={MainLayout}
+          component={Typography}
+        />
+        <RouteWithLayout path="/icons" layout={MainLayout} component={Icons} />
+        <RouteWithLayout
+          path="/account"
+          layout={MainLayout}
+          component={Account}
+        />
+        <RouteWithLayout
+          path="/settings"
+          layout={MainLayout}
+          component={Settings}
+        />
+        <RouteWithLayout
+          path="/sign-up"
+          layout={MainLayout}
+          component={SignUp}
+        />
+        <RouteWithLayout
+          path="/sign-in"
+          layout={MainLayout}
+          component={SignIn}
+        />
+        <RouteWithLayout
+          path="/sign-in"
+          layout={MainLayout}
+          component={SignIn}
+        />
+        <RouteWithLayout
+          path="/notfound"
+          layout={MainLayout}
+          component={NotFound}
+        />
       </Router>
     </ThemeProvider>
   );
