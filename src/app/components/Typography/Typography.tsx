eccab800be5b45f603f1f132c8d2937287a8c37a
nightillusions <pascal@jordin.eu>
2020-01-29 13:56:32 +0100

    :hammer: change imports for build

--- src/app/components/Typography/Typography.tsx
+++ src/app/components/Typography/Typography.tsx
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { Grid, Typography as MuiTypography } from '@material-ui/core';
 import { ITheme } from '../../theme';
-import ThemeStyle from '@material-ui/core/styles/createTypography';
+import { ThemeStyle } from '@material-ui/core/styles/createTypography';
 
 const useStyles = makeStyles((theme: ITheme) => ({
   root: {
@@ -19,13 +19,15 @@ const variants = {
   h6: 'Nisi euismod ante senectus consequat phasellus ut',
   subtitle1: 'Leo varius justo aptent arcu urna felis pede nisl',
   subtitle2: 'Leo varius justo aptent arcu urna felis pede nisl',
-  body1: 'Justo proin curabitur dictumst semper auctor, consequat tempor, nostra aenean neque turpis nunc. Leo. Sapien aliquet facilisi turpis, elit facilisi praesent porta metus leo. Dignissim amet dis nec ac integer inceptos erat dis Turpis sodales ad torquent. Dolor, erat convallis.Laoreet velit a fames commodo tristique hendrerit sociosqu rhoncus vel sapien penatibus facilisis faucibus ad. Mus purus vehicula imperdiet tempor lectus, feugiat Sapien erat viverra netus potenti mattis purus turpis. Interdum curabitur potenti tristique. Porta velit dignissim tristique ultrices primis.',
-  body2: 'Justo proin curabitur dictumst semper auctor, consequat tempor, nostra aenean neque turpis nunc. Leo. Sapien aliquet facilisi turpis, elit facilisi praesent porta metus leo. Dignissim amet dis nec ac integer inceptos erat dis Turpis sodales ad torquent. Dolor, erat convallis.',
+  body1:
+    'Justo proin curabitur dictumst semper auctor, consequat tempor, nostra aenean neque turpis nunc. Leo. Sapien aliquet facilisi turpis, elit facilisi praesent porta metus leo. Dignissim amet dis nec ac integer inceptos erat dis Turpis sodales ad torquent. Dolor, erat convallis.Laoreet velit a fames commodo tristique hendrerit sociosqu rhoncus vel sapien penatibus facilisis faucibus ad. Mus purus vehicula imperdiet tempor lectus, feugiat Sapien erat viverra netus potenti mattis purus turpis. Interdum curabitur potenti tristique. Porta velit dignissim tristique ultrices primis.',
+  body2:
+    'Justo proin curabitur dictumst semper auctor, consequat tempor, nostra aenean neque turpis nunc. Leo. Sapien aliquet facilisi turpis, elit facilisi praesent porta metus leo. Dignissim amet dis nec ac integer inceptos erat dis Turpis sodales ad torquent. Dolor, erat convallis.',
   caption: 'Accumsan leo pretium conubia ullamcorper.',
   overline: 'Accumsan leo pretium conubia ullamcorper.',
   button: 'Vivamus ultrices rutrum fames dictumst'
-} as { 
-  [key: string]: string
+} as {
+  [key: string]: string;
 };
 
 const Typography = () => {
@@ -33,25 +35,16 @@ const Typography = () => {
 
   return (
     <div className={classes.root}>
-      <Grid
-        container
-        spacing={4}
-      >
+      <Grid container spacing={4}>
         {Object.keys(variants).map((key, i) => (
           <Fragment key={i}>
-            <Grid
-              item
-              sm={3}
-              xs={12}
-            >
+            <Grid item sm={3} xs={12}>
               <MuiTypography variant="caption">{key}</MuiTypography>
             </Grid>
-            <Grid
-              item
-              sm={9}
-              xs={12}
-            >
-              <MuiTypography variant={(key as ThemeStyle | 'srOnly' | 'inherit')}>{variants[key]}</MuiTypography>
+            <Grid item sm={9} xs={12}>
+              <MuiTypography variant={key as ThemeStyle | 'srOnly' | 'inherit'}>
+                {variants[key]}
+              </MuiTypography>
             </Grid>
           </Fragment>
         ))}
