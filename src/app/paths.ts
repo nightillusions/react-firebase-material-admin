6ab065df78a205c2ef8ba30d778dc4e0bcce3a89
Pascal Jordin <pascal@jordin.eu>
2020-01-31 14:57:25 +0100

    :hammer: add path constant

---
+++ src/app/paths.ts
@@ -0,0 +1,19 @@
+  
+const rootPaths = {
+	ROOT: "/",
+	DASHBOARD: "/dashboard",
+	USERS: "/users",
+	PRODUCTS: "/products",
+	TYPOGRAPHY: "/typography",
+	ICONS: "/icons",
+	ACCOUNT: "/account",
+	SETTINGS: "/settings",
+	SIGN_UP: "/sign-up",
+	SIGN_IN: "/sign-in",
+	SIGN_OUT: "/sign-out",
+	NOT_FOUND: "/not-found",
+};
+
+export const PATHS = {
+	...rootPaths,
+};
