8718eec5fa5a934dd65558dae7abd793b35ce593
Pascal Jordin <pascal@jordin.eu>
2020-02-01 19:25:04 +0100

    :hammer: remove firestore create user

--- src/app/firestore/User.ts
+++ src/app/firestore/User.ts
@@ -1,12 +1,9 @@
 import { db } from "../firebase";
-import { IUserTemplate } from "../models/User";
 
 
 export const Users = {
-	create: async (user: IUserTemplate) => {
-		const userRef = await db.collection("users/").doc(user.id);
-		await userRef.set(user, { merge: true }).catch(error => { console.log(error.message); });
-		return userRef;
+	get: async (userId: string) => {
+		return (await db.collection("users/").doc(userId).get()).data;
 	}
 }
 
--- src/app/hooks/useAuth.ts
+++ src/app/hooks/useAuth.ts
@@ -1,9 +1,6 @@
 import { auth, User } from 'firebase';
 import { useState } from 'react';
-import getFirstName from '../../utils/getFirstName';
-import getLastName from '../../utils/getLastName';
 import FirebaseApp from '../firebase';
-import Users from '../firestore/User';
 
 const useAuth = (firebaseUser: User | null = null) => {
   const [user, setUser] = useState<User | null>(firebaseUser);
@@ -14,17 +11,6 @@ const useAuth = (firebaseUser: User | null = null) => {
     if (authUser) {
       // const token = await authUser.getIdToken(true);
       // console.log(`Bearer ${token}`);
-
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const user = await Users.create({
-        id: authUser.uid,
-        email: authUser.email ? authUser.email : "",
-        firstName: authUser.displayName ? getFirstName(authUser.displayName) : "",
-        lastName: authUser.displayName ? getLastName(authUser.displayName) : "",
-        phone: null,
-        avatarUrl: authUser.photoURL
-      })
-
       setUser(authUser);
       setPending(false);
 
