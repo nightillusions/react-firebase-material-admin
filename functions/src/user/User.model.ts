f26acbdf7c9e5309ed8eb212685121964cab5c52
Pascal Jordin <pascal@jordin.eu>
2020-02-01 21:35:36 +0100

    :hammer: extend user model

---
+++ functions/src/user/User.model.ts
@@ -0,0 +1,26 @@
+import { User } from "firebase";
+
+export interface IUserTemplate {
+	id: string;
+  email: string | null;
+  phone: string | null;
+	firstName: string | null;
+	lastName: string | null; 
+  avatarUrl: string | null;
+}
+
+export interface IUser extends IUserTemplate {
+	timezone: string | null;
+  address: IUserAddress | null;
+  updatedAt: number;
+  createdAt: number;
+}
+
+export interface IUserAddress {
+  country: string;
+  state: string;
+  city: string;
+  street: string;
+}
+
+export type IAppUser = IUser & User;
--- functions/src/user/createStoreUser.ts
+++ functions/src/user/createStoreUser.ts
@@ -2,22 +2,28 @@ import { UserRecord } from "firebase-functions/lib/providers/auth";
 import admin = require("firebase-admin");
 import getFirstName from "../utils/getFirstName";
 import getLastName from "../utils/getLastName";
+import { IUser } from "./User.model";
 
 const createStoreUser = async (authUser: UserRecord) => {
+	const user: IUser = {
+		id: authUser.uid,
+		email: authUser.email ? authUser.email : null,
+		firstName: authUser.displayName
+			? getFirstName(authUser.displayName)
+			: null,
+		lastName: authUser.displayName ? getLastName(authUser.displayName) : null,
+		phone: authUser.phoneNumber || null,
+		avatarUrl: authUser.photoURL || null,
+		address: null,
+		timezone: null,
+		updatedAt: Date.now(),
+		createdAt: Date.now()
+	}
 	await admin
 		.firestore()
 		.collection("users/")
-		.doc(authUser.uid)
-		.set({
-			id: authUser.uid,
-			email: authUser.email ? authUser.email : "",
-			firstName: authUser.displayName
-				? getFirstName(authUser.displayName)
-				: "",
-			lastName: authUser.displayName ? getLastName(authUser.displayName) : "",
-			phone: authUser.phoneNumber || null,
-			avatarUrl: authUser.photoURL || null
-		})
+		.doc(user.id)
+		.set(user)
 		.catch(error => {
 			console.error(error.message);
 		});
